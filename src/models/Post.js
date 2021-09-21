906f491bbe62b3b27f229ab81bb1f6727055a7a0
rafaeldev <rafaelsd1959@gmail.com>
2021-09-21 10:15:24 -0300

    api node

--- src/models/Post.js
+++ src/models/Post.js
@@ -12,7 +12,7 @@ const PostSchema = new Schema({
 });
 
 PostSchema.virtual('image_URL').get(function() {
-    return `${process.env.APP_URL}/files/${this.image}`
+    return `${process.env.APP_URL}files/${this.image}`
 })
 
 module.exports = model('Post', PostSchema);
